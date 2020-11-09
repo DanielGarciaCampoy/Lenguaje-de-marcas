a986cbfb6b32b321e77e7f8afb5a8a72c6cabbed
Daniel García Campoy <72436388+DanielGarciaCampoy@users.noreply.github.com>
2020-11-09 21:58:37 +0100

    Add files via upload

---
+++ Trabajos/halloween html/javascript/javascript.js
@@ -0,0 +1,8 @@
+/**/
+
+function sonar(sonido) {
+    document.getElementById(sonido).play();
+}
+function gif(identificador) {
+    document.getElementById(identificador).style.visibility = "visible";
+}
