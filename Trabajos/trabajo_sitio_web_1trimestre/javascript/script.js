c14b3b93321598b9f39af2c805bdf632e3d28a03
Daniel García Campoy <72436388+DanielGarciaCampoy@users.noreply.github.com>
2021-01-17 15:22:00 +0100

    Add files via upload

---
+++ Trabajos/trabajo_sitio_web_1trimestre/javascript/script.js
@@ -0,0 +1,17 @@
+/**funcion aclarar,oscurecer */
+
+function aclarar(opac) {
+  document.getElementById('img_script').style.opacity= opac;
+}
+
+function aclarar1(opac) {
+  document.getElementById('img_script1').style.opacity= opac;
+}
+
+function aclarar2(opac) {
+  document.getElementById('img_script2').style.opacity= opac;
+}
+
+function aclarar3(opac) {
+  document.getElementById('img_script3').style.opacity= opac;
+}
